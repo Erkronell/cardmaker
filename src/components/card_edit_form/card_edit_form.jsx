00c3d1ffeede0c283b77571e1c0966f6e62bbd11
Joon <cedielle@gmail.com>
2021-01-30 14:47:41 -0500

    add card add form component

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -25,9 +25,9 @@ const CardEditForm = ({ card }) => {
         value={company}
       />
       <select className={styles.select} name="theme" value={theme}>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-        <option value="colorful">Colorful</option>
+        <option value="light">light</option>
+        <option value="dark">dark</option>
+        <option value="colorful">colorful</option>
       </select>
       <input className={styles.input} type="text" name="title" value={title} />
       <input className={styles.input} type="text" name="email" value={email} />
@@ -39,7 +39,7 @@ const CardEditForm = ({ card }) => {
       <div className={styles.fileInput}>
         <ImageFileInput />
       </div>
-      <Button name="delete" onClick={onSubmit} />
+      <Button name="Delete" onClick={onSubmit} />
     </form>
   );
 };
